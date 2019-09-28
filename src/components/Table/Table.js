1037a13f9c65c3595337b0440ec99a42e7250862
Hugo Corta Lorenzo <corta.hugo@gmail.com>
2019-09-29 00:37:05 +0200

    Folder refactor

---
+++ src/components/Table/Table.js
@@ -0,0 +1,166 @@
+'use strict'
+
+import React from 'react'
+import memoize from 'memoize-one'
+import cx from 'classnames'
+import { utils } from '../../utils'
+import { defaultTableBodyRenderer } from './defaultTableBodyRenderer'
+import { defaultTableHeaderRenderer } from './defaultTableHeaderRenderer'
+import { defaultNoDataRenderer } from './defaultNoDataRenderer'
+import { defaultTablePaginationRenderer } from './defaultTablePaginationRenderer'
+import { withPagination } from './withPagination'
+import { withSortBy } from './withSortBy'
+
+
+const TableComponent = React.forwardRef((
+  {
+    className, headerClassName, rowClassName,
+    id, children, columns, data, style, width, height, defaultSorted, disabled, overscanRowCount,
+    tableHeaderRenderer, headerCellRenderer, headerHeight, headerRowRenderer, disableHeader, headerRowProps, headerCellProps, headerComponentProps, onHeaderClick,
+    tableBodyRenderer, rowRenderer, cellRenderer, rowHeight, rowProps, cellProps, cellComponentProps, onRowClick, onCellClick,
+    noDataRenderer, noDataComponent, noDataMessage, noDataProps, noDataComponentProps,
+    loading, loadingRenderer, loadingComponent,
+    onSortableClick, sortDirection, sortBy,
+    changePageTo, tablePaginationRenderer, paginationComponent, pagination, paginationProps, paginationHeight, pageSize, onNextPageClick, onPreviousPageClick, currentPage,
+    virtualized,
+    loadMoreRows, threshold,
+    ...rest
+  }, ref) => {
+
+  const tableBodyHeight = utils.calculateBodyHeight({
+    height,
+    rowHeight,
+    headerHeight,
+    disableHeader,
+    pagination,
+    paginationHeight,
+    pageSize,
+  })
+
+  const getColumns = memoize((columns, children) => {
+    return columns || utils.normalizeColumns(children)
+  })
+  const tableColumns = getColumns(columns, children)
+
+  function renderTableHeader(props) {
+    if (disableHeader) {
+      return null
+    } else {
+      return tableHeaderRenderer
+        ? tableHeaderRenderer(props)
+        : defaultTableHeaderRenderer(props)
+    }
+  }
+
+  function renderTableBody(props) {
+    if (!data || (data.length === 0 && !loading)) {
+      return renderNoData({
+        noDataComponent,
+        noDataMessage,
+        noDataProps,
+        noDataComponentProps,
+        tableBodyHeight
+      })
+    } else {
+      return tableBodyRenderer
+        ? tableBodyRenderer(props)
+        : defaultTableBodyRenderer(props)
+    }
+  }
+
+  function renderTableFooter() {
+
+  }
+
+  function renderTablePagination(props) {
+    return tablePaginationRenderer
+      ? tablePaginationRenderer(props)
+      : defaultTablePaginationRenderer(props)
+  }
+
+  function renderNoData(props) {
+    return noDataRenderer
+      ? noDataRenderer(props)
+      : defaultNoDataRenderer(props)
+  }
+
+  return (
+    <div
+      ref={ref}
+      id={id}
+      role={'table'}
+      className={cx('AwesomeTable', className)}
+      style={{
+        height,
+        width,
+        ...style
+      }}>
+      {renderTableHeader({
+        headerClassName,
+        columns: tableColumns,
+        headerRowProps,
+        headerCellProps,
+        disableHeader,
+        headerHeight,
+        headerComponentProps,
+        headerRowRenderer,
+        headerCellRenderer,
+        onHeaderClick,
+        onSortableClick,
+        sortDirection,
+        sortBy
+      })}
+      {renderTableBody({
+        rowClassName,
+        data,
+        rowHeight,
+        height,
+        columns: tableColumns,
+        rowProps,
+        cellProps,
+        cellComponentProps,
+        loading,
+        loadingComponent,
+        loadingRenderer,
+        rowRenderer,
+        cellRenderer,
+        onRowClick,
+        onCellClick,
+        tableBodyHeight,
+        virtualized,
+        overscanRowCount,
+        loadMoreRows,
+        threshold
+      })}
+      {renderTablePagination({
+        changePageTo,
+        pagination,
+        paginationHeight,
+        onNextPageClick,
+        onPreviousPageClick,
+        currentPage,
+        paginationComponent,
+        paginationProps
+      })}
+
+    </div>
+  )
+})
+
+export const Table = withSortBy(TableComponent)
+
+Table.defaultProps = {
+  virtualized: false,
+  disabled: false,
+  loading: false,
+  overscanRowCount: 10,
+  disableHeader: false,
+  columns: null,
+  data: null,
+  paginationHeight: 20,
+  rowHeight: 20,
+  headerHeight: 20,
+  footerHeight: 20,
+  loadMoreRows: null,
+  threshold: 10
+}
