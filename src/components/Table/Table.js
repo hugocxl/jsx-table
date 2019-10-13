2b3bf70a60c66ce73ac7d27b6dc820711c7d9d78
Hugo Corta Lorenzo <corta.hugo@gmail.com>
2019-10-13 15:31:50 +0200

    Table render refactor

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,15 +1,16 @@
 'use strict'
 
-import React from 'react'
-import memoize from 'memoize-one'
+import React, { useState, useRef, useEffect } from 'react'
 import cx from 'classnames'
+import { defaultLoadingRenderer } from './defaultLoadingRenderer'
 import { utils } from '../../utils'
-import { defaultTableBodyRenderer } from './defaultTableBodyRenderer'
+import { defaultTableRenderer } from './defaultTableRenderer'
 import { defaultTableHeaderRenderer } from './defaultTableHeaderRenderer'
 import { defaultNoDataRenderer } from './defaultNoDataRenderer'
 import { defaultTablePaginationRenderer } from './defaultTablePaginationRenderer'
 import { withPagination } from './withPagination'
 import { withSortBy } from './withSortBy'
+import { defaultRowRenderer } from './defaultRowRenderer'
 
 
 const TableComponent = React.forwardRef((
@@ -27,6 +28,8 @@ const TableComponent = React.forwardRef((
     ...rest
   }, ref) => {
 
+  const _ref = ref || useRef()
+  const [scrollTop, setScrollTop] = useState(null)
   const columnsData = columns || utils.normalizeColumns(children)
   const rowsData = getRowsData()
   const { computedRowGrid, rowWidth } = utils.computeRowGrid({ width, columns: columnsData })
@@ -39,6 +42,12 @@ const TableComponent = React.forwardRef((
     paginationHeight,
     pageSize,
   })
+
+  useEffect(() => {
+    if (virtualized) {
+      setScrollTop(scroll => scroll + 1)
+    }
+  }, [height])
 
   function getRowsData() {
     return data
@@ -54,7 +63,7 @@ const TableComponent = React.forwardRef((
     }
   }
 
-  function renderTableBody(props) {
+  function renderTableBody() {
     if (!data || (data.length === 0 && !loading)) {
       return renderNoData({
         noDataComponent,
@@ -64,9 +73,15 @@ const TableComponent = React.forwardRef((
         tableBodyHeight
       })
     } else {
-      return tableBodyRenderer
-        ? tableBodyRenderer(props)
-        : defaultTableBodyRenderer(props)
+      return (
+        <div style={{ height: data && (data.length * rowHeight) }}>
+          {virtualized ? (
+            renderVirtualizedRows()
+          ) : (
+            renderRows()
+          )}
+        </div>
+      )
     }
   }
 
@@ -86,57 +101,101 @@ const TableComponent = React.forwardRef((
       : defaultNoDataRenderer(props)
   }
 
+  function renderRows() {
+    return data.map((el, i) => {
+      const rowProps = {
+        rowData: el,
+        rowHeight,
+        rowIndex: i,
+        top: rowHeight * i,
+        ...rest
+      }
+
+      return rowRenderer
+        ? rowRenderer(rowProps)
+        : defaultRowRenderer(rowProps)
+    })
+  }
+
+  function renderVirtualizedRows() {
+    if (!_ref.current) {
+      return null
+    }
+
+    const { scrollTop, clientHeight } = _ref.current
+    const rows = []
+
+    const firstRowIndexCalc = Math.round(scrollTop / rowHeight) - overscanRowCount - 1
+    const lastRowIndexCalc = Math.round((scrollTop + clientHeight) / rowHeight) + overscanRowCount + 1
+
+    const firstRowIndex = firstRowIndexCalc > 0 ? firstRowIndexCalc : 0
+    const lastRowIndex = lastRowIndexCalc < data.length ? lastRowIndexCalc : data.length
+
+    for (let i = firstRowIndex; i < lastRowIndex; i++) {
+      const elProps = {
+        columns: columnsData,
+        rowData: data[i],
+        rowHeight,
+        rowIndex: i,
+        top: rowHeight * i,
+        computedRowGrid,
+        rowWidth,
+        ...rest
+      }
+      rows.push(
+        rowRenderer
+          ? rowRenderer(elProps)
+          : defaultRowRenderer(elProps)
+      )
+    }
+
+    return rows
+  }
+
+  function onTableScroll() {
+    const { scrollTop, clientHeight, scrollHeight } = _ref.current
+
+    // onScroll && onScroll({
+    //   scrollTop,
+    //   clientHeight,
+    //   scrollHeight
+    // })
+
+    setScrollTop(scrollTop)
+  }
+
   return (
     <div
-      ref={ref}
+      ref={_ref}
       id={id}
       role={'table'}
+      onScroll={onTableScroll}
       className={cx('AwesomeTable', className)}
       style={{
         height,
         width,
         ...style
       }}>
-      {renderTableHeader({
-        headerRowRenderer,
-        headerCellRenderer,
-        columns: columnsData,
-        computedRowGrid,
-        rowWidth,
-        headerClassName,
-        headerRowProps,
-        headerCellProps,
-        headerComponentProps,
-        disableHeader,
-        headerHeight,
-        onHeaderClick,
-        onSortableClick,
-        sortDirection,
-        sortBy
-      })}
-      {renderTableBody({
-        rowRenderer,
-        cellRenderer,
-        loadingRenderer,
-        data: rowsData,
-        columns: columnsData,
-        computedRowGrid,
-        height,
-        width,
-        rowHeight,
-        rowClassName,
-        rowWidth,
-        rowProps,
-        cellProps,
-        cellComponentProps,
-        loading,
-        loadingComponent,
-        onRowClick,
-        onCellClick,
-        tableBodyHeight,
-        virtualized,
-        overscanRowCount
-      })}
+      {/*{renderTableHeader({*/}
+      {/*  headerRowRenderer,*/}
+      {/*  headerCellRenderer,*/}
+      {/*  columns: columnsData,*/}
+      {/*  computedRowGrid,*/}
+      {/*  rowWidth,*/}
+      {/*  headerClassName,*/}
+      {/*  headerRowProps,*/}
+      {/*  headerCellProps,*/}
+      {/*  headerComponentProps,*/}
+      {/*  disableHeader,*/}
+      {/*  headerHeight,*/}
+      {/*  onHeaderClick,*/}
+      {/*  onSortableClick,*/}
+      {/*  sortDirection,*/}
+      {/*  sortBy*/}
+      {/*})}*/}
+ h
+      {renderTableBody()}
+
       {/*{renderTablePagination({*/}
       {/*  changePageTo,*/}
       {/*  pagination,*/}
